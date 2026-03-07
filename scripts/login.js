ec777c09d66c0ad0d02a3d824ac9e8ad81368b9e
juadul <parallaxhub@gmail.com>
2026-03-07 16:12:52 +0800

    Functionality added to the log in page

---
+++ scripts/login.js
@@ -0,0 +1,20 @@
+document.getElementById('login-btn')
+  .addEventListener("click",function(){
+    const userNameInput = document.getElementById('input-username');
+    const userName = userNameInput.value;
+    console.log(userName);
+    
+    const userPasswordInput = document.getElementById('input-password');
+    const userPassword = userPasswordInput.value;
+    console.log(userPassword);
+
+    if(userName == 'admin' && userPassword == 'admin123'){
+      alert('Sign In Successful');
+
+      window.location.assign("/homepage.html")
+    }else{
+      alert("Sig In Failed");
+      return;
+    }
+    
+  })
